22718e4d67864dfbded2f2592c42ba72352a6c57
Nick Lawrence <nick.lawrence@tempus.com>
2019-10-17 14:57:36 -0500

    adding go mod

--- deploy_build.sh
+++ deploy_build.sh
@@ -7,16 +7,10 @@ export GOBIN="$GOPATH/bin"
 echo "Cleaning previous compiled Go..."
 GOCACHE=$WORKSPACE go clean
 
-echo "Cleaning previous compiled Go..."
+echo "Installing from local Go mod..."
 GOCACHE=$WORKSPACE go mod download
 
-echo "Installing and building packages..."
-## Currently this only builds from the default branch
-## When we figure out our approach we can do:
-## "GOCACHE=$WORKSPACE go get github.com/tempuslabs/s3s2@GIT_VERSION_NUMBER"
-## to pull a specific version
-#GOCACHE=$WORKSPACE go get github.com/tempuslabs/s3s2
-
+echo "Building for operating systems..."
 GOOS=linux GOARCH=amd64 GOCACHE=$WORKSPACE go build -o linux/s3s2-linux-amd64 -v &
 GOOS=darwin GOARCH=amd64 GOCACHE=$WORKSPACE go build -o darwin/s3s2-darwin-amd64 -v &
 GOOS=windows GOARCH=amd64 GOCACHE=$WORKSPACE go build -o windows/s3s2-windows-amd64.exe -v &
