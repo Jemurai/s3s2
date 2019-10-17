8cb749e26830e7198878c2ac5abf10c843b259dc
Nick Lawrence <nick.lawrence@tempus.com>
2019-10-17 14:39:17 -0500

    - gave up on makefile and golang-alpine - go get needs git - go get does allow for @git-tag building, but did not figure out how to   do this intuitively

--- deploy_build.sh
+++ deploy_build.sh
@@ -8,6 +8,10 @@ echo "Cleaning previous compiled Go..."
 GOCACHE=$WORKSPACE go clean
 
 echo "Installing and building packages..."
+## Currently this only builds from the default branch
+## When we figure out our approach we can do:
+## "GOCACHE=$WORKSPACE go get github.com/tempuslabs/s3s2@GIT_VERSION_NUMBER"
+## to pull a specific version
 GOCACHE=$WORKSPACE go get github.com/tempuslabs/s3s2
 
 GOOS=linux GOARCH=amd64 GOCACHE=$WORKSPACE go build -o linux/s3s2-linux-amd64 -v &
