10cbe936c84b9c97067fe4e85f4f69b8fd3e5324
Nick Lawrence <nick.lawrence@tempus.com>
2020-07-14 13:57:20 -0500

    troubleshooting san error

--- deploy_build.sh
+++ deploy_build.sh
@@ -11,6 +11,6 @@ echo "Installing from local Go mod..."
 GOCACHE=$WORKSPACE go mod download
 
 echo "Building for operating systems..."
-GOOS=linux GOARCH=amd64 GOCACHE=$WORKSPACE go build -o linux/s3s2-linux-amd64 -v
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 GOCACHE=$WORKSPACE go build -o linux/s3s2-linux-amd64 -v
 GOOS=darwin GOARCH=amd64 GOCACHE=$WORKSPACE go build -o darwin/s3s2-darwin-amd64 -v
 GOOS=windows GOARCH=amd64 GOCACHE=$WORKSPACE go build -o windows/s3s2-windows-amd64.exe -v
